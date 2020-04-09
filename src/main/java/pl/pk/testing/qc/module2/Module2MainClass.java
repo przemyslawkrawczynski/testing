e8c0531b3b28b77ba1cda5efa2477869f1c72a77
unknown <pk@bluzo.pl>
2020-04-09 17:30:02 +0200

    2.2

---
+++ src/main/java/pl/pk/testing/qc/module2/Module2MainClass.java
@@ -0,0 +1,18 @@
+package pl.pk.testing.qc.module2;
+
+public class Module2MainClass {
+
+    public static void main(String[] args) {
+        twoTwoExercise();
+
+
+    }
+
+    public static void twoTwoExercise() {
+        System.out.println("PK");
+        System.out.println("P");
+        System.out.println(34);
+        System.out.println("K");
+    }
+
+}

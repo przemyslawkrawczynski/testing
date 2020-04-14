ff99fbe02f64babdabacc4a4ba24e7de0aa537f3
unknown <pk@bluzo.pl>
2020-04-14 19:56:40 +0200

    add Reflections library

--- src/main/java/pl/pk/testing/qc/module2/Module2MainClass.java
+++ src/main/java/pl/pk/testing/qc/module2/Module2MainClass.java
@@ -1,6 +1,23 @@
 package pl.pk.testing.qc.module2;
 
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class Module2MainClass {
+
+    public static void main(String[] args) {
+        classesInPackage().stream()
+                .map(Class::getName)
+                .forEach(System.out::println);
+    }
 
     public static void twoTwoExercise() {
         System.out.println("PK");
@@ -23,5 +40,21 @@ public class Module2MainClass {
         Notebook goodNotebook = new Notebook(2000, 1500, 2019);
         System.out.println(notebook.prepareOpinion());
         System.out.println(goodNotebook.checkPrice());
+    }
+
+    public static Set<Class<?>> classesInPackage() {
+        List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
+        classLoadersList.add(ClasspathHelper.contextClassLoader());
+        classLoadersList.add(ClasspathHelper.staticClassLoader());
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
+                .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("pl.pk.testing"))));
+
+//        Set<Class<? extends Object>> allClasses =
+//                reflections.getSubTypesOf(Object.class);
+
+        return reflections.getSubTypesOf(Object.class);
     }
 }
